72cf69922a4e02046fb7b33d3edcea177bcf50ca
JohannBillar <jwbillar@gmail.com>
2017-04-11 09:34:17 -0700

    Create project

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+require('./sass/main.scss');
+
+render(
+  <App message="Hello, world!" />,
+  document.getElementById('app')
+);
